b44a8114008cb4f1354dd0da6bcbc9976e7c3a21
iMario999 <iMario@outlook.com>
2015-05-04 10:08:56 -0400

    LOL

---
+++ H15_Projet_E01/H15_Projet_E01/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace H15_Projet_E01
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ H15_Projet_E01/H15_Projet_E01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="H15_Projet_E01.MvcApplication" Language="C#" %>
