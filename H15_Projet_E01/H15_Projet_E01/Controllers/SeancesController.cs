f55feac131eb195623e5edef40a63b7640138a48
kinpell <kinpell@hotmail.com>
2015-05-04 14:00:31 -0400

    controleur sceance générer

---
+++ H15_Projet_E01/H15_Projet_E01/Controllers/SeancesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using H15_Projet_E01.Models;
+
+namespace H15_Projet_E01.Controllers
+{
+    public class SeancesController : Controller
+    {
+        private PhotoDuvalEntities db = new PhotoDuvalEntities();
+
+        // GET: Seances
+        public ActionResult Index()
+        {
+            var seances = db.Seances.Include(s => s.Agent);
+            return View(seances.ToList());
+        }
+
+        // GET: Seances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seance seance = db.Seances.Find(id);
+            if (seance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seance);
+        }
+
+        // GET: Seances/Create
+        public ActionResult Create()
+        {
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "Nom");
+            return View();
+        }
+
+        // POST: Seances/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SeanceID,DateSeance,Adresse,Ville,Telephone,Commentaire,AgentID,FactureID,HeureSeance")] Seance seance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seances.Add(seance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "Nom", seance.AgentID);
+            return View(seance);
+        }
+
+        // GET: Seances/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seance seance = db.Seances.Find(id);
+            if (seance == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "Nom", seance.AgentID);
+            return View(seance);
+        }
+
+        // POST: Seances/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SeanceID,DateSeance,Adresse,Ville,Telephone,Commentaire,AgentID,FactureID,HeureSeance")] Seance seance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AgentID = new SelectList(db.Agents, "AgentID", "Nom", seance.AgentID);
+            return View(seance);
+        }
+
+        // GET: Seances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seance seance = db.Seances.Find(id);
+            if (seance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seance);
+        }
+
+        // POST: Seances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Seance seance = db.Seances.Find(id);
+            db.Seances.Remove(seance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
