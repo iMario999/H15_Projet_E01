e21b04c52ddd7abf07795c14333e376ac6ac0ae9
Mario <iMario@outlook.com>
2015-05-05 11:06:40 -0400

    Création de AgentMetaData, le controleur et les vues

---
+++ H15_Projet_E01/H15_Projet_E01/Models/AgentMetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace H15_Projet_E01.Models
+{
+    [MetadataType(typeof(AgentMetaData))]
+    public partial class Agent
+    {
+        internal sealed class AgentMetaData
+        {
+            public int AgentID { get; set; }
+            [Required]
+            public string Nom { get; set; }
+        }
+    }
+}
