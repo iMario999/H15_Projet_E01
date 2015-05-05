1c8ce8dbffb44be3fd062cf29a9fd2f185cd50e6
Mario <iMario@outlook.com>
2015-05-05 10:17:49 -0400

    Page d'introduction ;)

--- H15_Projet_E01/H15_Projet_E01/Controllers/HomeController.cs
+++ H15_Projet_E01/H15_Projet_E01/Controllers/HomeController.cs
@@ -22,7 +22,13 @@ namespace H15_Projet_E01.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "La technologie HDR attire l’œil en permettant la réalisation de photos de qualité ayant une luminosité exceptionnelle et un aperçu réaliste et vivant de la propriété.";
+
+            ViewBag.TitleHDR = "QU’EST-CE QUELA PHOTOGRAPHIE HDR?";
+            ViewBag.MessageHDR = "Le principe de base est de prendre de trois à neuf photos de la même scène avec un temps d’exposition différent. Ces photos sont par la suite traitées avec un logiciel spécialisé qui dé-termine la meilleure luminosité pour chacun des pixels de la photo. Ce qui assure une qualité d’image et une luminosité exceptionnelle.";
+
+            ViewBag.TitleOffre = "QU’OFFRONS-NOUS?";
+            ViewBag.MessageOffre = "Guy Duval et ses partenaires offrent un service professionnel avec garantie de satisfaction. Une gamme de produits est disponible pour la promotion de vos propriétés à des tarifs compétitifs et cet investissement est déductible d’impôt pour vous.";
 
             return View();
         }
