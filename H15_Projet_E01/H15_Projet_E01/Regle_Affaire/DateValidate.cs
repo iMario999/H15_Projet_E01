444b59193cfd0f1c72b11979ac9739c73d8a2c23
kyocanada <kyo.kaizuka@gmail.com>
2015-05-06 10:55:45 -0400

    lol

--- H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
+++ H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
@@ -17,25 +17,24 @@ namespace H15_Projet_E01.Regle_Affaire
                 if (seance.HeureSeance != null)
                     return new ValidationResult("Vous devez choisir la date ou laisser l'heure vide");
             }
-            else
-            {
-                if (seance.HeureSeance == null)
-                    return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
-                if (seance.DateSeance < DateTime.Now)
-                    return new ValidationResult("La date doit être dans le future");
-               
-                UnitOfWork unitOfWork = new UnitOfWork();
 
-                foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())
+            if (seance.HeureSeance == null)
+                return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
+            if (seance.DateSeance < DateTime.Now)
+                return new ValidationResult("La date doit être dans le future");
+               
+            UnitOfWork unitOfWork = new UnitOfWork();
+
+            foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())
+            {
+                if (s.DateSeance == seance.DateSeance)
                 {
-                    if (s.DateSeance == seance.DateSeance)
-                    {
-                        TimeSpan ts = (TimeSpan)(((DateTime)s.HeureSeance).TimeOfDay - ((DateTime)seance.HeureSeance).TimeOfDay);
-                        if (ts.Hours < 4 && ts.Hours > -4)
-                            return new ValidationResult("La différence entre deux séances en même journée devrait être plus de 4h. La séance la plus proche : " + s.DateSeance.Value.ToString("dd-MM-yyyy") + " " + s.HeureSeance.Value.ToString("HH:mm"));
-                    }
+                    TimeSpan ts = (TimeSpan)(((DateTime)s.HeureSeance).TimeOfDay - ((DateTime)seance.HeureSeance).TimeOfDay);
+                    if (ts.Hours < 4 && ts.Hours > -4)
+                        return new ValidationResult("La différence entre deux séances en même journée devrait être plus de 4h. La séance la plus proche : " + s.DateSeance.Value.ToString("dd-MM-yyyy") + " " + s.HeureSeance.Value.ToString("HH:mm"));
                 }
             }
+            
             return ValidationResult.Success;
 
         }
