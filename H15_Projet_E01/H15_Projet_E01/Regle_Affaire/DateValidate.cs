9c1f0666a6808f6228398195850d326115edb0c5
kyocanada <kyo.kaizuka@gmail.com>
2015-05-14 13:57:32 -0400

    Validation date fonctionne!!
    lorsqu'on modifie la séance passée mais que la date demeurre pareille,
    la validation passe automatiquement

--- H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
+++ H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
@@ -12,35 +12,44 @@ namespace H15_Projet_E01.Regle_Affaire
     {
         public static ValidationResult Validate(Seance seance)
         {
-            if (seance.DateSeance == null)
-            {
-                if (seance.HeureSeance != null)
-                    return new ValidationResult("Vous devez choisir la date ou laisser l'heure vide");
-            }
-            else
-            {
-                if (seance.HeureSeance == null)
-                    return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
-                if (seance.DateSeance <= DateTime.Now)
-                    return new ValidationResult("La date doit être dans le future");
-                if (seance.DateSeance > DateTime.Now.AddDays(15))
-                    return new ValidationResult("La date ne doit pas être plus loin que d'ici aujourd'hui + 15 jours");
+            UnitOfWork unitOfWork = new UnitOfWork();
 
-                UnitOfWork unitOfWork = new UnitOfWork();
+            Seance seanceAvantModif = unitOfWork.SeanceRepository.GetSeanceByID(seance.SeanceID);
 
-                foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())
+            if (seanceAvantModif.DateSeance != seance.DateSeance || seanceAvantModif.HeureSeance.Value.TimeOfDay != seance.HeureSeance.Value.TimeOfDay)
+                //il y a eu un changement de date ou heure
+            {
+                if (seance.DateSeance == null)
                 {
-                    if (s.DateSeance == seance.DateSeance)
+                    if (seance.HeureSeance != null)
+                        return new ValidationResult("Vous devez choisir la date ou laisser l'heure vide");
+                }
+                else
+                {
+                    if (seance.HeureSeance == null)
+                        return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
+                    if (seance.DateSeance <= DateTime.Now)
+                        return new ValidationResult("La date doit être dans le future");
+                    if (seance.DateSeance > DateTime.Now.AddDays(15))
+                        return new ValidationResult("La date ne doit pas être plus loin que d'ici aujourd'hui + 15 jours");
+
+
+                    foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())
                     {
-                        if (s.SeanceID != seance.SeanceID)
+                        if (s.DateSeance == seance.DateSeance)
                         {
-                            TimeSpan ts = (TimeSpan)(((DateTime)s.HeureSeance).TimeOfDay - ((DateTime)seance.HeureSeance).TimeOfDay);
-                            if (ts.Hours < 4 && ts.Hours > -4)
-                                return new ValidationResult("La différence entre deux séances en même journée devrait être plus de 4h. La séance la plus proche : " + s.DateSeance.Value.ToString("dd-MM-yyyy") + " " + s.HeureSeance.Value.ToString("HH:mm"));
+                            if (s.SeanceID != seance.SeanceID)
+                            {
+                                TimeSpan ts = (TimeSpan)(((DateTime)s.HeureSeance).TimeOfDay - ((DateTime)seance.HeureSeance).TimeOfDay);
+                                if (ts.Hours < 4 && ts.Hours > -4)
+                                    return new ValidationResult("La différence entre deux séances en même journée devrait être plus de 4h. La séance la plus proche : " + s.DateSeance.Value.ToString("dd-MM-yyyy") + " " + s.HeureSeance.Value.ToString("HH:mm"));
+                            }
                         }
                     }
                 }
             }
+
+
             
 
             
