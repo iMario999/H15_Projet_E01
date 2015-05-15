3a55ea2633123bc474043712a55847a7e8bff7c6
kyocanada <kyo.kaizuka@gmail.com>
2015-05-15 14:38:53 -0400

    debug validation

--- H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
+++ H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
@@ -17,13 +17,6 @@ namespace H15_Projet_E01.Regle_Affaire
             Seance seanceAvantModif = unitOfWork.SeanceRepository.GetSeanceByID(seance.SeanceID);
 
 
-            if (seanceAvantModif.DateSeance != seance.DateSeance || seanceAvantModif.HeureSeance.Value.TimeOfDay != seance.HeureSeance.Value.TimeOfDay)
-                //il y a eu un changement de date ou heure
-            {
-
-                if (seanceAvantModif.DateSeance < DateTime.Now)
-                    return new ValidationResult("Vous ne pouvez pas modifier la date de la séance déjà passée");
-
                 if (seance.DateSeance == null)
                 {
                     if (seance.HeureSeance != null)
@@ -31,27 +24,33 @@ namespace H15_Projet_E01.Regle_Affaire
                 }
                 else
                 {
-                    if (seance.HeureSeance == null)
-                        return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
-                    if (seance.DateSeance <= DateTime.Now)
-                        return new ValidationResult("La date doit être dans le future");
-                    if (seance.DateSeance > DateTime.Now.AddDays(15))
-                        return new ValidationResult("La date ne doit pas être plus loin que d'ici aujourd'hui + 15 jours");
-
-
-                    foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())
+                    if (seanceAvantModif.DateSeance != seance.DateSeance || seanceAvantModif.HeureSeance.Value.TimeOfDay != seance.HeureSeance.Value.TimeOfDay)
+                        //il y a eu un changement de date ou heure
                     {
-                        if (s.DateSeance == seance.DateSeance)
-                        {
-                            if (s.SeanceID != seance.SeanceID)
+                        if (seanceAvantModif.DateSeance < DateTime.Now)
+                            return new ValidationResult("Vous ne pouvez pas modifier la date de la séance déjà passée");
+
+                            if (seance.HeureSeance == null)
+                                return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
+                            if (seance.DateSeance <= DateTime.Now)
+                                return new ValidationResult("La date doit être dans le future");
+                            if (seance.DateSeance > DateTime.Now.AddDays(15))
+                                return new ValidationResult("La date ne doit pas être plus loin que d'ici aujourd'hui + 15 jours");
+
+
+                            foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())
                             {
-                                TimeSpan ts = (TimeSpan)(((DateTime)s.HeureSeance).TimeOfDay - ((DateTime)seance.HeureSeance).TimeOfDay);
-                                if (ts.Hours < 4 && ts.Hours > -4)
-                                    return new ValidationResult("La différence entre deux séances en même journée devrait être plus de 4h. La séance la plus proche : " + s.DateSeance.Value.ToString("dd-MM-yyyy") + " " + s.HeureSeance.Value.ToString("HH:mm"));
+                                if (s.DateSeance == seance.DateSeance)
+                                {
+                                    if (s.SeanceID != seance.SeanceID)
+                                    {
+                                        TimeSpan ts = (TimeSpan)(((DateTime)s.HeureSeance).TimeOfDay - ((DateTime)seance.HeureSeance).TimeOfDay);
+                                        if (ts.Hours < 4 && ts.Hours > -4)
+                                            return new ValidationResult("La différence entre deux séances en même journée devrait être plus de 4h. La séance la plus proche : " + s.DateSeance.Value.ToString("dd-MM-yyyy") + " " + s.HeureSeance.Value.ToString("HH:mm"));
+                                    }
+                                }
                             }
                         }
-                    }
-                }
             }
 
 
