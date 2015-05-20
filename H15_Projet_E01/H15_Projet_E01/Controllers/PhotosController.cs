67b9ef9b639c6931427cf50e864af8d094bb927e
Mario <iMario@outlook.com>
2015-05-20 17:04:35 -0400

    Ajouter les photos

---
+++ H15_Projet_E01/H15_Projet_E01/Controllers/PhotosController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using H15_Projet_E01.Models;
+using H15_Projet_E01.DAL;
+
+namespace H15_Projet_E01.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private H15_PROJET_E01Entities3 db = new H15_PROJET_E01Entities3();
+        private UnitOfWork unitOfWork = new UnitOfWork();
+
+        // GET: Photos
+        public ActionResult Index()
+        {
+            var photos = db.Photos.Include(p => p.Seance);
+            return View(photos.ToList());
+        }
+
+        // GET: Photos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // GET: Photos/Create
+        public ActionResult Create()
+        {
+            ViewBag.SeanceID = new SelectList(db.Seances, "SeanceID", "Adresse");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PhotoID,SeanceID,fileType,Path")] Photo photo, HttpPostedFileBase image)
+        {
+            //Check Directory if exist or else create
+            string subPath = "Images/" + photo.SeanceID;
+            bool exists = System.IO.Directory.Exists(Server.MapPath(subPath));
+            if (!exists)
+                System.IO.Directory.CreateDirectory(Server.MapPath(subPath));
+
+            //Add Path to Photo
+            photo.Path = "Photos/" + subPath + "/" + image.FileName;
+
+            //Add format type to Photo
+            string[] format = image.FileName.Split('.');
+            photo.fileType = format[format.Length - 1];
+
+            if (ModelState.IsValid)
+            { 
+                //Copy the picture into the folder
+                image.SaveAs(Server.MapPath(subPath) +"\\"+ image.FileName);
+
+                db.Photos.Add(photo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SeanceID = new SelectList(db.Seances, "SeanceID", "Adresse", photo.SeanceID);
+            return View(photo);
+        }
+
+        // GET: Photos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SeanceID = new SelectList(db.Seances, "SeanceID", "Adresse", photo.SeanceID);
+            return View(photo);
+        }
+
+        // POST: Photos/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PhotoID,SeanceID,fileType,Path")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(photo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SeanceID = new SelectList(db.Seances, "SeanceID", "Adresse", photo.SeanceID);
+            return View(photo);
+        }
+
+        // GET: Photos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            db.Photos.Remove(photo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
