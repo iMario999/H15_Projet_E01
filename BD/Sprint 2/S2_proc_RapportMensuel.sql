eaca32de81563d65821b8eb8621ac36b80272e21
kyocanada <kyo.kaizuka@gmail.com>
2015-05-20 15:05:41 -0400

    debug rapport mensuel

--- BD/Sprint 2/S2_proc_RapportMensuel.sql
+++ BD/Sprint 2/S2_proc_RapportMensuel.sql
@@ -11,8 +11,11 @@ BEGIN
 	SELECT A.AgentID, ISNULL(A.Prenom, ' ') + ' ' + ISNULL(A.Nom, ' ') AS Agent,  COUNT(SeanceID) AS 'NOMBRE DE SEANCE'
 	FROM Seance S
 	INNER JOIN Agent A ON A.AgentID = S.AgentID
+	WHERE MONTH(S.DateSeance) = @mois
+	AND YEAR(S.DateSeance) = @annee
 	GROUP BY A.AgentID, A.Prenom, A.Nom
-
 END
 GO
 
+--test pour voir le rapport de juin 2015
+--EXEC dbo.proc_rapportMensuel 6, 2015
