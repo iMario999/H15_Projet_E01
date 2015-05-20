d65a91878efb3230d0ac17fed7c28e8bcbf359ed
kyocanada <kyo.kaizuka@gmail.com>
2015-05-20 12:52:53 -0400

    modification de la validation date
    même si les deux séances ont la même date heure, si le photographe
    assigné est différent dans les deux séances, accepte

--- H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
+++ H15_Projet_E01/H15_Projet_E01/Regle_Affaire/DateValidate.cs
@@ -29,29 +29,32 @@ namespace H15_Projet_E01.Regle_Affaire
                     {
                         if (seanceAvantModif.DateSeance < DateTime.Now)
                             return new ValidationResult("Vous ne pouvez pas modifier la date de la séance déjà passée");
+                        if (seance.HeureSeance == null)
+                            return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
+                        if (seance.DateSeance <= DateTime.Now)
+                            return new ValidationResult("La date doit être dans le future");
+                        if (seance.DateSeance > DateTime.Now.AddDays(15))
+                            return new ValidationResult("La date ne doit pas être plus loin que d'ici aujourd'hui + 15 jours");
 
-                            if (seance.HeureSeance == null)
-                                return new ValidationResult("Vous devez choisir l'heure ou laisser la date vide");
-                            if (seance.DateSeance <= DateTime.Now)
-                                return new ValidationResult("La date doit être dans le future");
-                            if (seance.DateSeance > DateTime.Now.AddDays(15))
-                                return new ValidationResult("La date ne doit pas être plus loin que d'ici aujourd'hui + 15 jours");
 
-
-                            foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())
+                        foreach (Seance s in unitOfWork.SeanceRepository.GetSeances())//comparer avec tous les séances 
+                        {
+                            if (s.DateSeance == seance.DateSeance)//si les deux dates sont pareilles, valide
                             {
-                                if (s.DateSeance == seance.DateSeance)
+                                if (s.SeanceID != seance.SeanceID)//si c'est la même séance, skip
                                 {
-                                    if (s.SeanceID != seance.SeanceID)
+                                    if (s.PhotographeID == seance.PhotographeID)//si c'est le même photographe, valide
                                     {
                                         TimeSpan ts = (TimeSpan)(((DateTime)s.HeureSeance).TimeOfDay - ((DateTime)seance.HeureSeance).TimeOfDay);
-                                        if (ts.Hours < 4 && ts.Hours > -4)
+                                        if (ts.Hours < 4 && ts.Hours > -4)//si la différence est +- 4h, erreur
                                             return new ValidationResult("La différence entre deux séances en même journée devrait être plus de 4h. La séance la plus proche : " + s.DateSeance.Value.ToString("dd-MM-yyyy") + " " + s.HeureSeance.Value.ToString("HH:mm"));
+
                                     }
                                 }
                             }
                         }
-            }
+                    }
+                }
 
 
             
